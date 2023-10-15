c1b5e0ddcb8e4d23606ea4ed2b8c8621ea85fb4c
arifinlenna <arifin@lenna.ai>
2023-10-15 15:43:12 +0700

    update PDO exception

--- App.php
+++ App.php
@@ -1,11 +1,16 @@
 <?php
 
-$host="database";
-$port=5432;
+$host="localhost";
+$port=54320;
 $database="php-pzn";
 $username="postgres";
 $password="secret";
 
-$connection = new PDO("pgsql:host=$host;port=$port;dbname=$database",$username,$password);
 
-echo "sukses banget" . PHP_EOL;
+try {
+    $connection = new PDO("pgsql:host=$host;port=$port;dbname=$database",$username,$password);
+    
+    echo "sukses banget" . PHP_EOL;
+} catch (PDOException $e) {
+    echo "error: ".$e->getMessage();
+}
