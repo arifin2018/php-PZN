77880ab85615838f3ce4e0d4c52fe03594050eb3
arifinlenna <arifin@lenna.ai>
2023-10-21 09:14:11 +0700

    skip test and requires

--- test/CounterTest.php
+++ test/CounterTest.php
@@ -6,8 +6,10 @@ use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 final class CounterTest extends TestCase{
-
+    // ./vendor/bin/phpunit --filter CounterTest::testCounter test/CounterTest.php
+    // ./vendor/bin/phpunit test/CounterTest.php
     public function testCounter():Counter {
+        $this->markTestSkipped("");
         $counter = new Counter();
         $counter->increment();
         $counter->increment();
@@ -18,8 +20,21 @@ final class CounterTest extends TestCase{
 
     /**
      * @depends testCounter
+     * bergantung pada testCounter yang dimana dia akan melanjutkan dari testCounter lalu ke testCounter2
     */
     public function testCounter2(Counter $counter):void {
         Assert::assertEquals(3, $counter->getCounter());
+    }
+
+    public function testCounter3():void {
+        $counter = new Counter();
+        Assert::assertEquals(0, $counter->getCounter());
+    }
+
+    /**
+     * @requires PHP >= 8.0
+     */
+    public function testOnlyPHP8():void {
+        $this->assertTrue(true);
     }
 }
